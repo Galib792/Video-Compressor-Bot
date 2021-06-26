3af98537473a8aca7e38b4086568e91156be5497
eo792 <85933934+eo792@users.noreply.github.com>
2021-06-26 12:53:50 +0530

    Update new_join_fn.py

--- bot/plugins/new_join_fn.py
+++ bot/plugins/new_join_fn.py
@@ -54,7 +54,7 @@ async def help_message_f(client, message):
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
-                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
+                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/Tips_Tik")
                         ]
                     ]
                 ),
